e028a3d2519fe9a4c7e391dbf1781aded74d3542
Andile Ngwilingwili <ST10538326@rcconnect.edu.za>
2026-06-01 21:41:34 +0200

    Project restructure

---
+++ glms-backend-api/Models/ContractCreateViewModel.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.ComponentModel.DataAnnotations;
+using TechMove.Enums;
+
+namespace TechMove.Models
+{
+    public class ContractCreateViewModel
+    {
+        [Required]
+        public int ClientId { get; set; }
+
+        [Required]
+        [DataType(DataType.Date)]
+        public DateTime StartDate { get; set; }
+
+        [Required]
+        [DataType(DataType.Date)]
+        public DateTime EndDate { get; set; }
+
+        [Required]
+        public ContractStatus Status { get; set; }
+
+        [Required]
+        public ServiceLevel ServiceLevel { get; set; }
+
+        [Required]
+        [Display(Name = "Signed Agreement PDF")]
+        public IFormFile SignedAgreement { get; set; } = null!;
+    }
+}
